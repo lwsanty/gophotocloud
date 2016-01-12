dfdc6419e623a3a6bbb71a9258ee82668fd4d30b
lwsanty <lwsanty@gmail.com>
2016-01-12 12:34:51 +0200

    fixes, pass parses

--- icloud.go
+++ icloud.go
@@ -3,12 +3,23 @@ package main
 import (
 	"github.com/lwsanty/gophotocloud/engine"
 	//"github.com/lwsanty/gophotocloud/photos"
-	"github.com/lwsanty/gophotocloud/drive"
 	"fmt"
+	"github.com/bgentry/speakeasy"
+	"github.com/lwsanty/gophotocloud/drive"
 )
 
 func main() {
-	eng, err := engine.NewEngine("title", "cypher_")
+	login, err := speakeasy.Ask("enter your apple id: ")
+	if err != nil {
+		panic(err)
+	}
+
+	pass, err := speakeasy.Ask("enter your icloud pass: ")
+	if err != nil {
+		panic(err)
+	}
+
+	eng, err := engine.NewEngine(login, pass)
 	if err != nil {
 		panic(err)
 	}
@@ -26,38 +37,25 @@ func main() {
 	fmt.Println("================================================================")
 	fmt.Println("================================================================")
 	for i := range fitems_links.Items {
-		if fitems_links.Items[i].Type == "FOLDER" {
-			fitemz, _, _, err := drive.GetFolderItems(eng, fitems_links.Items[i].Id)
-			if err != nil {
-				panic(err)
-			}
-
-			fitemzz, err := drive.GetFileItemsUrls(fitemz, eng, cookie, token)
-			if err != nil {
-				panic(err)
-			}
-
-			fmt.Println("name: ", fitemzz.Items[i].Name)
-			fmt.Println("type: ", fitemzz.Items[i].Type)
-			fmt.Println("url: ", fitemzz.Items[i].Url)
-			break
-		}
+		fmt.Println("name: ", fitems.Items[i].Name)
+		fmt.Println("type: ", fitems.Items[i].Type)
+		fmt.Println("url: ", fitems.Items[i].Url)
 	}
 
 	/*
-	iclouddrive, err2 := drive.NewD(eng)
-	if err2 != nil {
-		panic(err2)
-	}
+		iclouddrive, err2 := drive.NewD(eng)
+		if err2 != nil {
+			panic(err2)
+		}
 
-	fmt.Println(iclouddrive.Urls)
+		fmt.Println(iclouddrive.Urls)
 
-	if err := photos.PrintContent(total); err != nil {
-		panic(err)
-	}
-
-		if err := photos.DownloadContent(total); err != nil {
+		if err := photos.PrintContent(total); err != nil {
 			panic(err)
 		}
+
+			if err := photos.DownloadContent(total); err != nil {
+				panic(err)
+			}
 	*/
 }
